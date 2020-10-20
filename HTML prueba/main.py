237e20865131feabc457693bbeed5da6461c6ad2
Wpelaez <willmarpelaez58@gmail.com>
2020-10-19 20:45:27 -0500

    Segunda Subida

--- HTML prueba/main.py
+++ HTML prueba/main.py
@@ -7,25 +7,33 @@ app = Flask(__name__)
 def index():
     return render_template("menus.html")
 
-@app.route("/wpc")
+@app.route("/wpc_principal")
 def wpc_principal():
     return render_template("wpc/principal.html")
 
-@app.route("/datos")
+@app.route("/wpc_datos")
 def wpc_datos():
     return render_template("wpc/datos.html")
 
-@app.route("/infoe")
+@app.route("/wpc_infoe")
 def wpc_infoe():
     return render_template("wpc/infoe.html")
 
-@app.route("/tecno")
+@app.route("/wpc_tecno")
 def wpc_tecno():
     return render_template("wpc/tecno.html")
 
-@app.route("/fav")
+@app.route("/fav_principal")
 def fav_principal():
-    return 'Pendiente'
+    return render_template("fav/Hola.html")
+
+@app.route("/fav_Hola2")
+def fav_Hola2():
+    return render_template("fav/Hola2.html")
+
+@app.route("/fav_Hola3")
+def fav_Hola3():
+    return render_template("fav/Hola3.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
